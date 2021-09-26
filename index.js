de0175de68175f6777d5079b8c6b206748505cf2
Mickael FOSSO <fosso.mickael@gmail.com>
2021-09-26 22:28:13 +0200

    Api with current user returned

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 require('./models/User');
 require('./services/passport');
 
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [process.env.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
+
 const dotenv = require('dotenv');
 dotenv.config();
 
--- services/passport.js
+++ services/passport.js
@@ -7,12 +7,35 @@ dotenv.config();
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+})
+
 passport.use(new GoogleStrategy({
     clientID: process.env.googleClientID,
     clientSecret: process.env.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id}).save();
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+            if (existingUser)
+            {
+                done(null, existingUser);
+            }
+            else
+            {
+                new User({ googleId: profile.id})
+                .save()
+                .then(user => done(null,user));
+            }
+        })
+        
 
 }));
 
