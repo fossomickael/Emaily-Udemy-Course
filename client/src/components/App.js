fb6aafdd4c1362c6ad13d01ec6a19ea0526ac08b
Mickael FOSSO <fosso.mickael@gmail.com>
2021-09-27 19:17:37 +0200

    React routes and components

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component={Landing} />
+                    <Route path="/surveys" component={Dashboard} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -23,20 +23,13 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id }).then((existingUser) => {
+    }, async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id })
             if (existingUser)
             {
-                done(null, existingUser);
+                return done(null, existingUser);
             }
-            else
-            {
-                new User({ googleId: profile.id})
-                .save()
-                .then(user => done(null,user));
-            }
-        })
-        
-
+            const user = await new User({ googleId: profile.id}).save();
+            done(null,user);  
 }));
 
