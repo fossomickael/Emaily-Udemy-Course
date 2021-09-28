08bdef94dc3bbd3718f4799ea70bc43e88257452
Mickael FOSSO <fosso.mickael@gmail.com>
2021-09-28 16:36:56 +0200

    Added client

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Payments from './Payments';
+
 class Header extends Component {
     renderContent() {
        switch (this.props.auth) {
@@ -14,6 +15,7 @@ class Header extends Component {
         default:
             return [
             <li key="1"><Payments/></li>,
+            <li key="3">Credits {this.props.auth.credits} </li>,
             <li key="2"><a href="/api/logout">Logout</a></li>
             ];
 
--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE === 'production') {
+  app.use(express.static('/client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+  })
+}
+
 const dotenv = require('dotenv');
 dotenv.config();
 
