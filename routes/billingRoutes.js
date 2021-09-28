b36f7a30985d3d064b3ceb56a6b7cbb25170e1a6
Mickael FOSSO <fosso.mickael@gmail.com>
2021-09-28 10:19:08 +0200

    finalizing charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,12 +1,12 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
-        console.log(req.body);
-        stripe.charges.create({
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id,
           });
+
     })
 };
